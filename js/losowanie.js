6a75cdc33786428531dfac0a437737ca2abed26c
sliwek-1 <msliwinski247@gmail.com>
2024-04-21 13:17:19 +0200

    #

--- js/losowanie.js
+++ js/losowanie.js
@@ -237,13 +237,42 @@ export function sprawdzanie(element,poprawne){
 // funkcja zlicza poprawne odpowiedzi gdzy odpowiedz użydkownika do komkretnego pytania jest zgodna z poprawną
 // nadaje odpowiednie klasy dla dobrej oraz złej odpowiedzi ponadto zlicza procentowy wynik oraz przekazuje te parametry do funkcji wyświetlającej wynik
 function sprawdzCzyOdpowiedziUzytkownikaPoprawne(odp, poprawne){
+    let answersList = document.querySelectorAll('.answers-list');
     let result = 0;
     let examID = document.querySelector('.exam-id').value;
     let id = examID.slice(0,5);
 
     
-    checkIdIsValid();
-    colorAnswersAfterChecking(odp, poprawne)
+    checkIdIsValid()
+
+    answersList.forEach((ans, i) => {
+        let answers = ans.querySelectorAll('.answer');
+        answers.forEach(answer => {
+            if(answer.textContent == odp[i].odp){
+                //console.log(i ,answer.textContent, user_odp[i]?.odp)
+                if(poprawne[i].poprawna == odp[i]?.odp){
+                    answer.classList.add('odpgood');
+                    result++;
+                }else{
+                    answer.classList.add('odpbad');
+                    let answersElement = answer.parentElement;
+                    let otherAnswers = [...answersElement.querySelectorAll('.answer')];
+                    otherAnswers.forEach(answer => {
+                        if(answer.textContent == poprawne[i].poprawna){
+                                answer.classList.add('active');
+                        }
+                    })
+                }
+            }
+
+           if(odp[i]?.odp == "Brak odpowiedzi"){  
+                if(answer.textContent == poprawne[i].poprawna){
+                    //console.log(poprawne[i].poprawna)
+                    answer.style.background = 'royalblue';
+                }
+            }
+        })
+    })
 
     let wynik = (result/parseInt(poprawne.length) * 100).toFixed(1);
     pokazWynik(result,poprawne.length,wynik);
